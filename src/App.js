9b25faf0716f1f2c0244a99c3362b7fc054f6fce
Ilya Sovenko <ilyasovenko@gmail.com>
2018-04-08 15:28:08 +0300

    start developing first section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo-white.svg';
 import * as styles from './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
-          <h1 className={styles['App-title']}>Welcome to React</h1>
-        </header>
-        <p className={styles['App-intro']}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className={styles.section}>
+          <img src={logo} alt="logo"/>
+        </div>
       </div>
     );
   }
