6e04b0e8f2bd494ef7353ea9cd1ae0b0123fb574
Ilya Sovenko <ilyasovenko@gmail.com>
2018-04-09 22:55:01 +0300

    few code changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <div className={styles.section}>
+        <div className={styles.firstSection}>
           <img src={logo} alt="logo"/>
         </div>
       </div>
